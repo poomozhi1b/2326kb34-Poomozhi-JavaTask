d8f76bd156b24e53a35fad41fe44b399d9ce4219
poomozhi1b <163979099+poomozhi1b@users.noreply.github.com>
2025-06-15 23:40:24 +0530

    Add files via upload

---
+++ day_3/UniqueEmail.java
@@ -0,0 +1,23 @@
+package day_3;
+
+import java.util.HashSet;
+
+public class UniqueEmail {
+
+    public static void main(String[] args) {
+        
+        HashSet<String> emailAddresses = new HashSet<>();
+
+        
+        emailAddresses.add("alice@example.com");
+        emailAddresses.add("bob@example.com");
+        emailAddresses.add("charlie@example.com");
+        emailAddresses.add("alice@example.com");  
+
+       
+        System.out.println("Unique Email Addresses:");
+        for (String email : emailAddresses) {
+            System.out.println(email);
+        }
+    }
+}
